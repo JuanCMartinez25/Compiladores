02dbfe064cf017a566024e3be097ec1cdf5c84b5
JuanCMartinez25 <133995534+JuanCMartinez25@users.noreply.github.com>
2023-06-08 07:57:51 -0600

    Create puntero.c

---
+++ puntero.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+    int u = 3;
+    int v;
+    int *pu;
+    int *pv;
+
+    pu = &u;
+    v = *pu;
+    pv = &v;
+
+    printf("\nu=%d &u=%p pu=%p *pu=%d", u, &u, pu, *pu);
+    printf("\n\nv=%d &v=%p pv=%p *pv=%d", v, &v, pv, *pv);
+}
